0762f7d7656089bf9cfb82df39984ba9973019fa
JoeJ1 <joeedwardharry1@gmail.com>
2017-06-16 13:34:48 +0000

    big commit of buttons menu

--- text-based_game.py
+++ text-based_game.py
@@ -64,6 +64,58 @@ C7a = str("???")
 C8a = str("???")
 #Defining skill menu function
 
+def buttonsmenu():
+    global choice
+	def button1(option):
+        choice = str(option)
+    def button2(option):
+        choice = str(option)
+    def button3(option):
+        choice = str(option)
+    def button4(option):
+        choice = str(option)
+    def button5(option):
+        choice = str(option)
+    def button6(option):
+        choice = str(option)
+
+    #the variables for changing the choices of each button
+    c1 = str("1")
+    c2 = str("2")
+    c3 = str("3")
+    c4 = str("4")
+    c5 = str("5")
+    c6 = str("6")
+
+    root = Tk()
+    root.title("Controller")
+    root.geometry("300x200")
+    app = Frame(root)
+    app.pack({"side":"left"})
+    bttn1 = Button(app, command=button1(c1))
+    bttn1["text"] = c1
+    bttn2 = Button(app, command=button2(c2))
+    bttn2["text"] = c2
+    bttn3 = Button(app, command=button3(c3))
+    bttn3["text"] = c3
+    bttn4 = Button(app, command=button4(c4))
+    bttn4["text"] = c4
+    bttn5 = Button(app, command=button5(c5))
+    bttn5["text"] = c5
+    bttn6 = Button(app, command=button6(c6))
+    bttn6["text"] = c6
+    bttn2.pack({"side":"right"})
+    bttn5.pack({"side":"left"})
+    bttn6.pack({"side":"left"})
+    bttn5.pack({"padx":"15"})
+    bttn6.pack({"padx":"15"})
+    bttn4.pack({"side":"left"})
+    bttn1.pack({"side":"top"})
+    bttn3.pack({"side":"bottom"})
+	return(choice)
+
+    root.mainloop()
+
 def skillslide():
     global skillpoints,skill
     def confirm(): #necissary to have this defined in here, stupid but it works
@@ -140,6 +192,7 @@ def skillmenu():
     clear(8)
     if(skillpoints>0):
         print("Which skill do you want to change?")
+		buttonsmenu()
         if(choice == "1"):
             print("""
                                                                                                                                ``
